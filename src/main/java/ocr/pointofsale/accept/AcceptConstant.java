26eb8d7ffcba55fe8b1a9747e6193682228190a1
pcitc <pcitc@10.163.225.105>
2016-12-12 16:40:48 +0800

    增加了门店收货通知

---
+++ src/main/java/ocr/pointofsale/accept/AcceptConstant.java
@@ -0,0 +1,16 @@
+package ocr.pointofsale.accept;
+
+/**
+ * 收货通知常量类
+ * @author wanghw
+ *
+ */
+public class AcceptConstant {
+
+	//收货通知创建地址
+	public static final String CREATE_ADDRESS = "create";
+	
+	//创建状态
+	public static final String CREATE_STATUS = "create";
+
+}
---
+++ src/main/java/ocr/pointofsale/accept/AcceptCreateHandler.java
@@ -0,0 +1,61 @@
+package ocr.pointofsale.accept;
+
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import ocr.common.handler.SampleBillBaseHandler;
+import otocloud.common.ActionURI;
+import otocloud.framework.app.function.ActionDescriptor;
+import otocloud.framework.app.function.AppActivityImpl;
+import otocloud.framework.app.function.BizRootType;
+import otocloud.framework.app.function.BizStateSwitchDesc;
+import otocloud.framework.core.HandlerDescriptor;
+
+/**
+ * 收货通知创建操作
+ * 
+ * @author wanghw
+ *
+ */
+public class AcceptCreateHandler extends SampleBillBaseHandler{
+
+	public AcceptCreateHandler(AppActivityImpl appActivity) {
+		super(appActivity);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * corecorp_setting.setting
+	 */
+	@Override
+	public String getEventAddress() {
+		return AcceptConstant.CREATE_ADDRESS;
+	}
+
+	@Override
+	public String getPartnerAcct(JsonObject bo) {
+		String partnerAcct = bo.getJsonObject("supplier").getString("link_account"); 
+		return partnerAcct;
+	}	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ActionDescriptor getActionDesc() {
+
+		ActionDescriptor actionDescriptor = super.getActionDesc();
+		HandlerDescriptor handlerDescriptor = actionDescriptor.getHandlerDescriptor();
+				
+		ActionURI uri = new ActionURI(AcceptConstant.CREATE_ADDRESS, HttpMethod.POST);
+		handlerDescriptor.setRestApiURI(uri);
+		
+		//状态变化定义
+		BizStateSwitchDesc bizStateSwitchDesc = new BizStateSwitchDesc(BizRootType.BIZ_OBJECT, 
+				null, AcceptConstant.CREATE_STATUS);
+		bizStateSwitchDesc.setWebExpose(true); //是否向web端发布事件		
+		actionDescriptor.setBizStateSwitch(bizStateSwitchDesc);
+		
+		return actionDescriptor;
+	}
+
+}
