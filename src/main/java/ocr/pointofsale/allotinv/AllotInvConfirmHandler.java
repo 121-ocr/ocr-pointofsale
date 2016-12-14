ae0ef1e22cb5a6161f24e214ca9a8e6eca6f3d12
pcitc <pcitc@pcitc-PC>
2016-12-14 23:11:27 +0800

    增加了保存价格单

--- src/main/java/ocr/pointofsale/allotinv/AllotInvConfirmHandler.java
+++ src/main/java/ocr/pointofsale/allotinv/AllotInvConfirmHandler.java
@@ -51,10 +51,9 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 		// 更新补货单
 		JsonObject replenishment = msg.body().getJsonObject("replenishment");
 		// 创建价格表
-		JsonArray prices = getPricesObject(replenishment);
 		Future<JsonObject> priceFuture = Future.future();
 		futures.add(priceFuture);
-		createPrices(prices, priceFuture);
+		createPrices(replenishment, priceFuture);
 		// 更新现存量
 		JsonArray invOnhand = getInvOnhandObject(replenishment);
 		Future<JsonObject> invOnhandFuture = Future.future();
@@ -75,13 +74,14 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 					}
 				}
 			}
-			// 更新补货单			
-			updateReplenishment(replenishment,msg);
+			// 更新补货单
+			updateReplenishment(replenishment, msg);
 		});
 	}
 
 	/**
 	 * 保存现存量
+	 * 
 	 * @param prices
 	 * @param invOnhandFuture
 	 */
@@ -99,11 +99,12 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 				appActivity.getLogger().error(errMsgString, errThrowable);
 				invOnhandFuture.fail(invRet.cause());
 			}
-		});		
+		});
 	}
 
 	/**
 	 * 获取现存量VO
+	 * 
 	 * @param replenishment
 	 * @return
 	 */
@@ -119,21 +120,22 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 			param.put("warehousecode", replenishment.getJsonObject("target_warehose").getString("code"));
 			Double conhandnum = 0.0;
 			JsonArray replenishment_s = detailO.getJsonArray("shipments");
-			if(replenishment_s == null || replenishment_s.isEmpty()){
+			if (replenishment_s == null || replenishment_s.isEmpty()) {
 				continue;
 			}
 			for (Object object2 : replenishment_s) {
 				JsonObject detail_s = (JsonObject) object2;
-				if(detail_s.getBoolean("accept_completed")){
-					conhandnum = conhandnum.doubleValue() + Double.valueOf(detailO.getString("accept_quantity")).doubleValue();
-				}				
+				if (detail_s.getBoolean("accept_completed")) {
+					conhandnum = conhandnum.doubleValue()
+							+ Double.valueOf(detailO.getString("accept_quantity")).doubleValue();
+				}
 			}
 			param.put("onhandnum", conhandnum.toString());
 			param.put("goodaccount", detailO.getJsonObject("goods").getString("account"));
-			
-			if(conhandnum >0){
+
+			if (conhandnum > 0) {
 				paramList.add(param);
-			}			
+			}
 		}
 		return paramList;
 	}
@@ -141,59 +143,101 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 	/**
 	 * 保存价格表
 	 * 
-	 * @param prices
-	 * @param priceFuture
-	 */
-	private void createPrices(JsonArray prices, Future<JsonObject> priceFuture) {
-		String from_account = this.appActivity.getAppInstContext().getAccount();
-		// 创建门店价格表
-		String getPriceAddress = from_account + "." + this.appActivity.getService().getRealServiceName()
-				+ ".posprice.create";
-		this.appActivity.getEventBus().send(getPriceAddress, prices, invRet -> {
-			if (invRet.succeeded()) {
-				priceFuture.complete();
-			} else {
-				Throwable errThrowable = invRet.cause();
-				String errMsgString = errThrowable.getMessage();
-				appActivity.getLogger().error(errMsgString, errThrowable);
-				priceFuture.fail(invRet.cause());
-			}
-		});
-	}
-
-	/**
-	 * 通过补货单构造价格单
-	 * 
 	 * @param replenishment
 	 * @return
 	 */
-	private JsonArray getPricesObject(JsonObject replenishment) {
-		// 保存门店价格表
-		JsonArray prices = new JsonArray();
-		// 构建prices
+	private void createPrices(JsonObject replenishment, Future<JsonObject> priceFuture) {
 		JsonArray details = replenishment.getJsonArray("details");
-		//TODO 需要判断已经存在的价格表不需要再保存了
-		
-		for (Object detail : details) {
-			JsonObject detail_obj = (JsonObject) detail;
-			JsonObject price = new JsonObject();
+		JsonObject query = new JsonObject();
+		JsonObject query_con = new JsonObject();
+		JsonArray query_item = new JsonArray();
+		query_con.put("$or", query_item);
+		// 首先按照sku+货主+批次查询已存在的价格
+		for (int i = 0; i < details.size(); i++) {
+			JsonObject detail_obj = (JsonObject) details.getValue(i);
+			JsonObject temp = new JsonObject();
+			temp.put("goods.product_sku_code", detail_obj.getJsonObject("goods").getString("product_sku_code"));
+			temp.put("invbatchcode", detail_obj.getString("invbatchcode"));
+			temp.put("goods.account", replenishment.getString("account"));
 
-			price.put("goods", detail_obj.getJsonObject("goods"));
-			price.put("invbatchcode", detail_obj.getString("invbatchcode"));
-			price.put("supply_price", detail_obj.getJsonObject("supply_price"));
-			price.put("retail_price", detail_obj.getJsonObject("retail_price"));
-			price.put("commission", detail_obj.getJsonObject("commission"));
-
-			prices.add(price);
+			query_item.add(temp);
 		}
-		return prices;
+		query.put("query", query_con);
+		String from_account = this.appActivity.getAppInstContext().getAccount();
+		// 创建门店价格表
+		String getPriceAddress = from_account + "." + this.appActivity.getService().getRealServiceName()
+				+ ".posprice.query";
+		List<Future> futures = new ArrayList<>();
+
+		this.appActivity.getEventBus().send(getPriceAddress, query, invRet -> {
+			if (invRet.succeeded()) {
+				// 已存在的价格
+				JsonObject data = (JsonObject) invRet.result();
+				JsonArray datas = data.getJsonArray("datas");
+				// 构建prices
+				for (Object detail : details) {
+					JsonObject detail_obj = (JsonObject) detail;
+					JsonObject price = new JsonObject();
+
+					price.put("goods", detail_obj.getJsonObject("goods"));
+					price.put("invbatchcode", detail_obj.getString("invbatchcode"));
+					price.put("supply_price", detail_obj.getJsonObject("supply_price"));
+					price.put("retail_price", detail_obj.getJsonObject("retail_price"));
+					price.put("commission", detail_obj.getJsonObject("commission"));
+
+					boolean isExist = false;
+					for (Object obj : datas) {
+						JsonObject temp = (JsonObject) obj;
+						if (price.getJsonObject("goods").getString("product_sku_code")
+								.equals(temp.getJsonObject("goods").getString("product_sku_code"))
+								&& price.getJsonObject("goods").getString("accoutn")
+										.equals(temp.getJsonObject("goods").getString("accoutn"))
+								&& price.getString("invbatchcode").equals(temp.getString("invbatchcode"))) {
+							isExist = true;
+							break;
+						}
+					}
+					if (isExist) {
+						continue;
+					}
+					Future<JsonObject> future = Future.future();
+					futures.add(future);
+					future.complete(price);
+				}
+			}
+		});
+		// 组合
+		CompositeFuture.join(futures).setHandler(ar -> {
+			JsonArray prices = new JsonArray();
+			CompositeFutureImpl comFutures = (CompositeFutureImpl) ar;
+			if (comFutures.size() > 0) {
+				for (int i = 0; i < comFutures.size(); i++) {
+					if (comFutures.succeeded(i)) {
+						prices.add(comFutures.result());
+					}
+				}
+			}
+			// 创建门店价格表
+			String createPriceAddress = from_account + "." + this.appActivity.getService().getRealServiceName()
+					+ ".posprice.create";
+			this.appActivity.getEventBus().send(createPriceAddress, prices, invRet -> {
+				if (invRet.succeeded()) {
+					priceFuture.complete();
+				} else {
+					Throwable errThrowable = invRet.cause();
+					String errMsgString = errThrowable.getMessage();
+					appActivity.getLogger().error(errMsgString, errThrowable);
+					priceFuture.fail(invRet.cause());
+				}
+			});
+		});
 	}
 
 	/**
 	 * 更新补货单
 	 * 
 	 * @param replenishment
-	 * @param msg 
+	 * @param msg
 	 * @param replenishmentFuture
 	 */
 	private void updateReplenishment(JsonObject replenishment, OtoCloudBusMessage<JsonObject> msg) {
@@ -201,17 +245,17 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 		String invSrvName = this.appActivity.getDependencies().getJsonObject("salescenter_service")
 				.getString("service_name", "");
 		String getReplenishmentAddress = from_account + "." + invSrvName + "." + "channel-restocking.update4accept";
-		//修改收货通知的标识
+		// 修改收货通知的标识
 		JsonArray replenishment_b = replenishment.getJsonArray("details");
 		for (Object object : replenishment_b) {
 			JsonObject detail = (JsonObject) object;
 			JsonArray replenishment_s = detail.getJsonArray("shipments");
-			if(replenishment_s == null || replenishment_s.isEmpty()){
+			if (replenishment_s == null || replenishment_s.isEmpty()) {
 				continue;
 			}
 			for (Object object2 : replenishment_s) {
 				JsonObject detail_s = (JsonObject) object2;
-				if(detail_s.getBoolean("accept_completed")){
+				if (detail_s.getBoolean("accept_completed")) {
 					continue;
 				}
 				detail_s.put("accept_completed", true);
@@ -224,7 +268,7 @@ public class AllotInvConfirmHandler extends ActionHandlerImpl<JsonObject> {
 				Throwable errThrowable = ret.cause();
 				String errMsgString = errThrowable.getMessage();
 				appActivity.getLogger().error(errMsgString, errThrowable);
-				msg.fail(100,errMsgString);
+				msg.fail(100, errMsgString);
 			}
 		});
 	}
