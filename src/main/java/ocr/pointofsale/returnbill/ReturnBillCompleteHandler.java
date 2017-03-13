f98e6f182dc078e943604ad039c06c8dd582bdfd
pcitc <pcitc@10.163.225.50>
2017-03-13 10:01:22 +0800

    退货单完成时入库

--- src/main/java/ocr/pointofsale/returnbill/ReturnBillCompleteHandler.java
+++ src/main/java/ocr/pointofsale/returnbill/ReturnBillCompleteHandler.java
@@ -1,6 +1,9 @@
 package ocr.pointofsale.returnbill;
 
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import ocr.common.handler.SampleCDOBillBaseHandler;
 import otocloud.common.ActionURI;
 import otocloud.framework.app.function.ActionDescriptor;
@@ -49,5 +52,39 @@ public class ReturnBillCompleteHandler extends SampleCDOBillBaseHandler{
 		actionDescriptor.setBizStateSwitch(bizStateSwitchDesc);
 		
 		return actionDescriptor;
+	}
+	
+	@Override
+	protected void afterProcess(JsonObject bo, Future<JsonObject> future) {
+		String from_account = this.appActivity.getAppInstContext().getAccount();
+		JsonArray paramList = new JsonArray();
+		for (Object detail : bo.getJsonArray("detail")) {
+			JsonObject param = new JsonObject();
+			JsonObject detailO = (JsonObject) detail;
+			param.put("warehouses", detailO.getJsonObject("stocking_warehouse"));
+			param.put("goods", detailO.getJsonObject("goods"));
+			param.put("sku", detailO.getJsonObject("goods").getString("product_sku_code"));
+			param.put("invbatchcode", detailO.getString("batch_code"));
+			param.put("shelf_life", detailO.getString("shelf_life"));
+			param.put("warehousecode", detailO.getJsonObject("stocking_warehouse").getString("code"));
+			param.put("onhandnum", detailO.getDouble("quantity"));
+			param.put("goodaccount", detailO.getJsonObject("goods").getString("account"));
+			param.put("status", "IN");
+			param.put("biz_data_type", "bp_returnbill");
+			param.put("bo_id", bo.getString("bo_id"));
+
+			paramList.add(param);
+		}
+		// 增加现存量，调用现存量的接口
+		String invSrvName = this.appActivity.getDependencies().getJsonObject("inventorycenter_service")
+				.getString("service_name", "");
+		String getWarehouseAddress = from_account + "." + invSrvName + "." + "stockonhand-mgr.batchcreate";
+		this.appActivity.getEventBus().send(getWarehouseAddress, paramList, invRet -> {
+			if (invRet.succeeded()) {
+				future.complete(bo);
+			} else {
+				future.fail(invRet.cause());
+			}
+		});
 	}
 }
