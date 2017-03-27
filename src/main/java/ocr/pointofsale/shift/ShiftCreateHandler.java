5b32dc5f6b32c9f93ab52c007bcb20a4d6a43f62
window <window@10.16.82.13>
2017-03-27 17:08:12 +0800

    轮班+零售单汇总

---
+++ src/main/java/ocr/pointofsale/shift/ShiftCreateHandler.java
@@ -0,0 +1,55 @@
+package ocr.pointofsale.shift;
+
+import io.vertx.core.http.HttpMethod;
+import ocr.common.handler.SampleBillBaseHandler;
+import otocloud.common.ActionURI;
+import otocloud.framework.app.function.ActionDescriptor;
+import otocloud.framework.app.function.AppActivityImpl;
+import otocloud.framework.app.function.BizRootType;
+import otocloud.framework.app.function.BizStateSwitchDesc;
+import otocloud.framework.core.HandlerDescriptor;
+
+/**
+ * 交班创建
+ * 
+ *
+ */
+public class ShiftCreateHandler extends SampleBillBaseHandler {
+
+	public ShiftCreateHandler(AppActivityImpl appActivity) {
+		super(appActivity);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * corecorp_setting.setting
+	 */
+	@Override
+	public String getEventAddress() {
+		return ShiftConstant.CREATE_ADDRESS;
+	}
+
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ActionDescriptor getActionDesc() {
+
+		ActionDescriptor actionDescriptor = super.getActionDesc();
+		HandlerDescriptor handlerDescriptor = actionDescriptor.getHandlerDescriptor();
+
+		// 外部访问url定义
+		ActionURI uri = new ActionURI(getEventAddress(), HttpMethod.POST);
+		handlerDescriptor.setRestApiURI(uri);
+
+		// 状态变化定义
+		BizStateSwitchDesc bizStateSwitchDesc = new BizStateSwitchDesc(BizRootType.BIZ_OBJECT, null,
+				ShiftConstant.CREATE_STATUS);
+		bizStateSwitchDesc.setWebExpose(true); // 是否向web端发布事件
+		actionDescriptor.setBizStateSwitch(bizStateSwitchDesc);
+
+		return actionDescriptor;
+	}
+
+}
